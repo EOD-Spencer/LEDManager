a3e3b116619b22edf02a1c293f145ba04bd54d30
Cody Spencer <EOD.Spencer@iCloud.com>
2026-09-02 16:45:46 -0400

    docs: correct package metadata for consolidated project

--- setup.py
+++ setup.py
@@ -51,14 +51,20 @@ extensions = [
 ]
 
 setup(
+    # Keep the historical package/CLI name for compatibility with the inherited
+    # LED Manager application even though the repository is now consolidated.
     name='led-control',
     version='2.0.0',
-    description='WS2812 LED strip controller with web interface for Raspberry Pi',
+    description='Web-controlled RGBW LED system with Arduino USB serial firmware',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='jackw01',
+    author='Cody Spencer / EOD-Spencer; derived from work by jackw01',
     python_requires='>=3.7.0',
-    url='https://github.com/jackw01/led-control',
+    url='https://github.com/EOD-Spencer/LEDManager',
+    project_urls={
+        'Source': 'https://github.com/EOD-Spencer/LEDManager',
+        'Upstream led-control': 'https://github.com/jackw01/led-control',
+    },
     packages=find_packages(),
     zip_safe=False,
     install_requires=requirements,
@@ -76,8 +82,6 @@ setup(
     },
     license='MIT',
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
